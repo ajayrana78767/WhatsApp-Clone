672d2e14c6ab9568c479deeea7ee64322efe16dd
ajayrana78767 <ajayrana78767@gmail.com>
2024-05-20 22:26:25 +0530

    Chat Screen Created
    I can create a chat screen but now it is not fully created.

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:whatsapp_clone/utils/colors.dart';
+import 'package:whatsapp_clone/utils/spacing_utils.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -10,8 +13,140 @@ class ChatsScreen extends StatefulWidget {
 class _ChatsScreenState extends State<ChatsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Chats Screen'),),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 20.0, top: 10, bottom: 20, right: 20),
+          child: ListView(
+
+         children: [ Column(
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      'WhatsApp',
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: primaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const Expanded(child: SpacingUtils.space10),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.qr_code_scanner,
+                          size: 25,
+                          color: blackColor,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.camera_alt_outlined,
+                          size: 25,
+                          color: blackColor,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.more_vert,
+                          size: 25,
+                          color: blackColor,
+                        )),
+                  ],
+                ),
+                SpacingUtils.space10,
+                TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color.fromRGBO(246, 245, 243, 1),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      size: 25,
+                      color: Color.fromRGBO(106, 115, 120, 1),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 10.0),
+                    hintText: 'Search...',
+                  ),
+                ),
+                SpacingUtils.space20,
+                Row(
+                  children: [
+                    ElevatedButton(onPressed: () {}, child: const Text('All')),
+                    SpacingUtils.wspace5,
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text('Unread')),
+                    SpacingUtils.wspace5,
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text('Group')),
+                  ],
+                ),
+                SpacingUtils.space20,
+                ListTile(
+                  onTap: () {},
+                  leading: SvgPicture.asset(
+                      height: 25, width: 25, 'assets/images/archived_icon.svg'),
+                  title: const Text(
+                    'Archived',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: blackColor),
+                  ),
+                ),
+                ListView.separated(
+                    //scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemBuilder: (index, context) {
+                      return ListTile(
+                        onTap: () {},
+                        leading: SvgPicture.asset(
+                            height: 50,
+                            width: 50,
+                            'assets/images/profile_icon.svg'),
+                        title: const Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                'Chat 1',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: blackColor),
+                              ),
+                            ),
+                            Text('10:00 pm',style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                    color: blackColor),),
+                          ],
+                        ),
+                        subtitle: const Row(
+                          children: [
+                            Expanded(
+                                child: Text('Last Chat Shows.....')),
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(
+                        height: 20,
+                      );
+                    },
+                    itemCount: 15)
+              ],
+            ),
+         ],
+          ),
+        
+        ),
+      ),
     );
   }
 }
