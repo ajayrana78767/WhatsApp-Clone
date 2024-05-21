e19c951b84d6e5159a0ed63e96d352291ecdb4bc
ajayrana78767 <ajayrana78767@gmail.com>
2024-05-21 22:10:24 +0530

    Chat Screen & Logo, Name Changed.

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -16,135 +16,139 @@ class _ChatsScreenState extends State<ChatsScreen> {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding:
-              const EdgeInsets.only(left: 20.0, top: 10, bottom: 20, right: 20),
-          child: ListView(
-
-         children: [ Column(
-              children: [
-                Row(
+          padding: const EdgeInsets.only(left: 20.0, top: 10, bottom: 20, right: 20),
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Column(
                   children: [
-                    const Text(
-                      'WhatsApp',
-                      style: TextStyle(
-                          fontSize: 24,
-                          color: primaryColor,
-                          fontWeight: FontWeight.bold),
+                   // Uncomment the following lines if needed
+                    Row(
+                      children: [
+                        const Text(
+                          'WhatsApp',
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: primaryColor,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const Expanded(child: SpacingUtils.space10),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.qr_code_scanner,
+                              size: 25,
+                              color: blackColor,
+                            )),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.camera_alt_outlined,
+                              size: 25,
+                              color: blackColor,
+                            )),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.more_vert,
+                              size: 25,
+                              color: blackColor,
+                            )),
+                      ],
                     ),
-                    const Expanded(child: SpacingUtils.space10),
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.qr_code_scanner,
+                    SpacingUtils.space10,
+                    TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: const Color.fromRGBO(246, 245, 243, 1),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.search,
                           size: 25,
-                          color: blackColor,
-                        )),
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.camera_alt_outlined,
-                          size: 25,
-                          color: blackColor,
-                        )),
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.more_vert,
-                          size: 25,
-                          color: blackColor,
-                        )),
+                          color: Color.fromRGBO(106, 115, 120, 1),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 10.0),
+                        hintText: 'Search...',
+                      ),
+                    ),
+                    SpacingUtils.space20,
+                    Row(
+                      children: [
+                        ElevatedButton(onPressed: () {}, child: const Text('All')),
+                        SpacingUtils.wspace5,
+                        ElevatedButton(
+                            onPressed: () {}, child: const Text('Unread')),
+                        SpacingUtils.wspace5,
+                        ElevatedButton(
+                            onPressed: () {}, child: const Text('Group')),
+                      ],
+                    ),
+                    SpacingUtils.space20,
+                    ListTile(
+                      onTap: () {},
+                      leading: SvgPicture.asset(
+                          height: 25, width: 25, 'assets/images/archived_icon.svg'),
+                      title: const Text(
+                        'Archived',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: blackColor),
+                      ),
+                    ),
                   ],
                 ),
-                SpacingUtils.space10,
-                TextFormField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: const Color.fromRGBO(246, 245, 243, 1),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    prefixIcon: const Icon(
-                      Icons.search,
-                      size: 25,
-                      color: Color.fromRGBO(106, 115, 120, 1),
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 10.0),
-                    hintText: 'Search...',
-                  ),
-                ),
-                SpacingUtils.space20,
-                Row(
-                  children: [
-                    ElevatedButton(onPressed: () {}, child: const Text('All')),
-                    SpacingUtils.wspace5,
-                    ElevatedButton(
-                        onPressed: () {}, child: const Text('Unread')),
-                    SpacingUtils.wspace5,
-                    ElevatedButton(
-                        onPressed: () {}, child: const Text('Group')),
-                  ],
-                ),
-                SpacingUtils.space20,
-                ListTile(
-                  onTap: () {},
-                  leading: SvgPicture.asset(
-                      height: 25, width: 25, 'assets/images/archived_icon.svg'),
-                  title: const Text(
-                    'Archived',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: blackColor),
-                  ),
-                ),
-                ListView.separated(
-                    //scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemBuilder: (index, context) {
-                      return ListTile(
-                        onTap: () {},
-                        leading: SvgPicture.asset(
-                            height: 50,
-                            width: 50,
-                            'assets/images/profile_icon.svg'),
-                        title: const Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                'Chat 1',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: blackColor),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return ListTile(
+                      onTap: () {},
+                      leading: SvgPicture.asset(
+                        'assets/images/profile_icon.svg',
+                        height: 50,
+                        width: 50,
+                      ),
+                      title: const Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              'Chat 1',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: blackColor,
                               ),
                             ),
-                            Text('10:00 pm',style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.normal,
-                                    color: blackColor),),
-                          ],
-                        ),
-                        subtitle: const Row(
-                          children: [
-                            Expanded(
-                                child: Text('Last Chat Shows.....')),
-                          ],
-                        ),
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return const SizedBox(
-                        height: 20,
-                      );
-                    },
-                    itemCount: 15)
-              ],
-            ),
-         ],
+                          ),
+                          Text(
+                            '10:00 pm',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                              color: blackColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      subtitle: const Row(
+                        children: [
+                          Expanded(
+                            child: Text('Last Chat Shows.....'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  childCount: 15,
+                ),
+              ),
+            ],
           ),
-        
         ),
       ),
     );
